315c346d180040ee86479a657dd2699c1f8ecb73
wvv <winxos@xhu.edu.cn>
2022-06-21 14:35:56 +0800

    logging to utf8

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pyttsx3
 import logging
 logger = logging.getLogger("simple")
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler("log.txt")
+fh = logging.FileHandler("log.txt", encoding="utf-8")
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter("[%(asctime)s][%(levelname)s]%(message)s", '%Y-%m-%d %H:%M:%S')
 fh.setFormatter(formatter)
